cb1a920963950ae8dbd10e075bb0d0552500b437
krumelist <sontic.due@gmail.com>
2017-12-29 15:56:12 +0100

    removed webpackdevmiddleware

--- Startup.cs
+++ Startup.cs
@@ -30,14 +30,14 @@ namespace AzureToolkit {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
-                    HotModuleReplacement = true
-                });
-            } else {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            // if (env.IsDevelopment()) {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
+            //         HotModuleReplacement = true
+            //     });
+            // } else {
+            //     app.UseExceptionHandler("/Home/Error");
+            // }
 
             app.UseStaticFiles();
 
